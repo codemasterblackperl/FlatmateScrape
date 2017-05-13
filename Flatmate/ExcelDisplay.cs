8473de8759e175775df53684a251cca74a0e8d62
Ajit <codemasterblackperl@outlook.com>
2017-05-13 15:05:34 +0530

    added excel loading

---
+++ Flatmate/ExcelDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flatmate
+{
+    public class ExcelDisplay
+    {
+        public string SubUrb { get; set; }
+        public string State { get; set; }
+        public string Done { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+    }
+}
--- Flatmate/MainWindow.xaml.cs
+++ Flatmate/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +26,96 @@ namespace Flatmate
         {
             InitializeComponent();
         }
+
+        private FastCollection<ExcelDisplay> _fcSuburbs;
+
+        private bool _isProcessing;
+        private string _excelFile;
+
+        
+        private void BtnLoadExcel_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = "*.xlsx";
+            dlg.Multiselect = false;
+            var dlgRes = dlg.ShowDialog();
+            if (dlgRes == false)
+                return;
+            _excelFile = dlg.FileName;
+            LoadExcelSheet();
+        }
+
+        private void BtnStartWork_Click(object sender, RoutedEventArgs e)
+        {
+            if (_fcSuburbs.Count == 0)
+            {
+                MessageBox.Show("Please load excel sheet before starting process", "Info");
+                return;
+            }
+
+            _isProcessing = true;
+            BtnStopWork.IsEnabled = true;
+
+            
+        }
+
+        private void BtnStopWork_Click(object sender, RoutedEventArgs e)
+        {
+            _isProcessing = false;
+            BtnStopWork.IsEnabled = false;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Init();
+        }
+
+        async private void Init()
+        {
+            _fcSuburbs = new FastCollection<ExcelDisplay>();
+            DgExcelSheet.IsReadOnly = true;
+            DgExcelSheet.ItemsSource = _fcSuburbs;
+            DgExcelSheet.MinColumnWidth = 50;
+            _isProcessing = false;
+            BtnStopWork.IsEnabled = _isProcessing;
+        }
+
+        private void UpdateLog(string text)
+        {
+            var message = "[ " + DateTime.Now.ToString() + " ]:  " + text+"\r\n";
+            TxtLog.AppendText(message);
+            if (TxtLog.Text.Length > 100000)
+            {
+                TxtLog.Text.Remove(0, 20000);
+            }
+            TxtLog.ScrollToEnd();
+        }
+
+        async private void LoadExcelSheet()
+        {
+            try
+            {
+                using (ExcelPackage ep = new ExcelPackage(new FileInfo(_excelFile)))
+                {
+                    var suburbsSheet = ep.Workbook.Worksheets["SuburbList"];
+                    var rowCount = suburbsSheet.Dimension.End.Row;
+
+                    for(int i = 0; i < rowCount; i++)
+                    {
+                        var excelDisp = new ExcelDisplay
+                        {
+                            SubUrb = suburbsSheet.Cells[i, 0].Value.ToString(),
+                            State=suburbsSheet.Cells[i,1].Value.ToString()
+                        };
+                        _fcSuburbs.Add(excelDisp);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
     }
 }
